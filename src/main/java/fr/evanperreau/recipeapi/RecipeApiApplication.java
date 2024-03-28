c00cbe5ca31f706d5c4cf6a235b7dca6b7166fa8
Evan <evan.perreau@gmail.com>
2024-03-28 18:24:30 +0100

    Implement CommandLineRunner To the App class

--- src/main/java/fr/evanperreau/recipeapi/RecipeApiApplication.java
+++ src/main/java/fr/evanperreau/recipeapi/RecipeApiApplication.java
@@ -1,13 +1,19 @@
 package fr.evanperreau.recipeapi;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RecipeApiApplication {
+public class RecipeApiApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(RecipeApiApplication.class, args);
     }
 
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Application started.");
+    }
 }
