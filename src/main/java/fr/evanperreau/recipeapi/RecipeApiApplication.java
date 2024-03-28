df8829b411392155fd29b40f9c261af9993262e0
Evan <evan.perreau@gmail.com>
2024-03-28 17:55:36 +0100

    init commit

---
+++ src/main/java/fr/evanperreau/recipeapi/RecipeApiApplication.java
@@ -0,0 +1,13 @@
+package fr.evanperreau.recipeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeApiApplication.class, args);
+    }
+
+}
